46fd58d8165032c7dbd6a12da8454fe054d052fc
Jiang <jiangshen95@163.com>
2018-03-26 08:03:59 +0800

    add Dungeon Game

---
+++ DungeonGame.py
@@ -0,0 +1,30 @@
+class Solution:
+    def calculateMinimumHP(self, dungeon):
+        """
+        :type dungeon: List[List[int]]
+        :rtype: int
+        """
+        m = len(dungeon)
+        n = len(dungeon[0])
+        dp = [0] * n
+        dp[n-1] = max(1, 1 - dungeon[m-1][n-1])
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                if i == m-1 and j == n-1:
+                    continue
+                if i == m-1:
+                    dp[j] = max(1, dp[j+1] - dungeon[i][j])
+                elif j == n-1:
+                    dp[j] = max(1, dp[j] - dungeon[i][j])
+                else:
+                    dp[j] = max(1, min(dp[j], dp[j+1]) - dungeon[i][j])
+        return dp[0]
+        
+if __name__=='__main__':
+    m = int(raw_input())
+    dungeon = []
+    for i in range(m):
+        raw = list(input())
+        dungeon.append(raw)
+    solution = Solution()
+    print(solution.calculateMinimumHP(dungeon))
---
+++ DungeonGame2.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int> >& dungeon) {
+        
+        int m = dungeon.size(), n = dungeon[0].size();
+        
+        vector<int> dp(n);
+        dp[n-1] = max(1, 1 - dungeon[m-1][n-1]);
+        
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i == m-1 && j == n-1){
+                    continue;
+                }
+                if(i == m-1){
+                    dp[j] = max(1, dp[j+1] - dungeon[i][j]);
+                }else if(j == n-1){
+                    dp[j] = max(1, dp[j] - dungeon[i][j]);
+                }else{
+                    dp[j] = max(min(dp[j], dp[j+1]) - dungeon[i][j], 1);
+                }
+            }
+        }
+        
+        return dp[0];
+        
+    }
+};
+
+int main(){
+    
+    int m, n;
+    cin>>m>>n;
+    
+    vector<vector<int> > dungeon;
+    for(int i=0;i<m;i++){
+        vector<int> raw;
+        for(int j=0;j<n;j++){
+            int num;
+            cin>>num;
+            raw.push_back(num);
+        }
+        dungeon.push_back(raw);
+    }
+    
+    Solution *solution = new Solution();
+    cout<<solution->calculateMinimumHP(dungeon);
+    
+    return 0;
+    
+}

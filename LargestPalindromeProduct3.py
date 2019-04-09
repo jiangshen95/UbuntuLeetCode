9ebc4b78d24cc8d4da3d971716131c06e5e5314d
Jiang <jiangshen95@163.com>
2019-04-09 11:21:05 +0800

    supple Largest Palindrome Product

--- LargestPalindromeProduct3.py
+++ LargestPalindromeProduct3.py
@@ -7,8 +7,12 @@ class Solution:
             right = int(str(left)[::-1])
             if z ** 2 - 4 * right < 0:
                 continue
-            if (z ** 2 - 4 * right) ** 0.5 == int((z ** 2 - 4 * right) ** 0.5):
+            x = 1 / 2 * (z + (z ** 2 - 4 * right) ** 0.5)
+            y = 1 / 2 * (z - (z ** 2 - 4 * right) ** 0.5)
+            if x.is_integer() or y.is_integer():
                 return (10 ** n * left + right) % 1337
+            # if (z ** 2 - 4 * right) ** 0.5 == int((z ** 2 - 4 * right) ** 0.5):
+            #     return (10 ** n * left + right) % 1337
 
 
 if __name__ == '__main__':
